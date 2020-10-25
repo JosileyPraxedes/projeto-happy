2208065a25552bf8f18174b0f3f194e351521038
JosileyPraxedes <56873688+JosileyPraxedes@users.noreply.github.com>
2020-10-25 18:55:16 -0300

    update orphanages.html
    creating the map part using Leaflet.js

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,38 @@
+// create map
+const map = L.map("mapid").setView([-22.89119, -43.09189], 14);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+}).addTo(map);
+
+// create icon
+const myIcon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  maxWidth: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>'
+);
+
+L.marker([-22.8757, -43.08], { icon: myIcon })
+  .addTo(map) // Orfanato Santo Antônio
+  .bindPopup(popup);
+
+// Lar da criança Padre Franz Neumair
+var marker = L.marker([-22.8993, -43.0795]).addTo(map);
+
+// Quintal da Ana
+var marker = L.marker([-22.9144, -43.0855]).addTo(map);
+
+// Sempre criança - Ponto de encontro
+var marker = L.marker([-22.9039, -43.0975]).addTo(map);
